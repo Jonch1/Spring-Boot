21f364e040bda1cce33655ee9a7a52cf1f4be9a3
Juanjo <juanjose.s435736@cesurformacion.com>
2025-01-23 10:20:07 +0100

    Primer Spring

--- src/main/java/com/example/primerspring/Book.java
+++ src/main/java/com/example/primerspring/Book.java
@@ -1,4 +1,8 @@
 package com.example.primerspring;
 
-public record Book(Long id, String title) {
+public record Book(Long id, String title, String author) {
+
+    public long getId() {
+        return id;
+    }
 }
--- src/main/java/com/example/primerspring/BookController.java
+++ src/main/java/com/example/primerspring/BookController.java
@@ -2,27 +2,43 @@ package com.example.primerspring;
 
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/api/books")
 @CrossOrigin(origins = {"http://localhost:4200/"})
+
 public class BookController {
 
+    //Lista de todos los libros disponibles
+    List<Book> books = List.of(
+            new Book(1L, "book1", "Antonio"),
+            new Book(2L, "book2", "Federico"),
+            new Book(3L, "book3", "Alfredo"),
+            new Book(4L, "Mi Libro :)", "Juanjo")
+    );
+
+    //Metodo findAll()
     @GetMapping
     public List<Book> findAll(){
         System.out.println("findAll");
-        return List.of(
-                new Book(1L, "book1"),
-                new Book(2L, "book2"),
-                new Book(3L, "book3"),
-                new Book(4L, "Mi Libro :)")
-
-        );
+        return books;
     }
 
+    //Metodo findById()
     @GetMapping("/{id}")
-    public Book findById(@PathVariable long id){
-        System.out.println("findById "+id);
-        return new Book(id, "Book example");
+    public Stream<Book> findById(@PathVariable long id) {
+        System.out.println("findById " + id);
+
+        // Buscar el libro por ID (mediante stream se procesan los datos
+        // de la lista de libros y luego con filter filtramos por id
+        return books.stream().filter(book -> book.getId() == id);
     }
+
+    @GetMapping("/Autores")
+    public List<Book> findAutores(){
+        System.out.println("findAutores");
+        return books;
+    }
+
 }
